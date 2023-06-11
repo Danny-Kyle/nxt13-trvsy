081f5b5aeed9861d0e9c0de61a84ac7fe8f9dd87
Okechukwu Daniel <88836879+Danny-Kyle@users.noreply.github.com>
2023-06-11 21:37:30 +0100

    init commit

---
+++ app/about/team/page.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Meet the Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        facilisis nibh id lacus pretium scelerisque. Nam eleifend quis dolor et
+        congue. Aliquam non mauris nisl. Orci varius natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus. Quisque hendrerit ante
+        nec elit auctor, nec auctor lorem suscipit. Pellentesque nec ante et
+        magna bibendum pharetra ac id erat. Praesent tempor leo eget facilisis
+        eleifend. Aenean egestas tellus elementum, vestibulum elit quis, rutrum
+        risus. Aliquam vulputate, neque sollicitudin tempus dignissim, est
+        mauris fringilla ante, vitae consectetur lectus metus id ex. Nam non
+        euismod ex, at rhoncus lacus. Etiam ut mattis tellus. Phasellus
+        vestibulum aliquet nulla quis vulputate. Phasellus risus enim, faucibus
+        et orci euismod, condimentum accumsan augue. Aenean tortor tellus,
+        bibendum non orci in, convallis iaculis ex. Nulla scelerisque, tellus ut
+        faucibus congue, arcu tellus rhoncus purus, id malesuada tellus dui sed
+        mi. Nunc nec aliquam odio. Donec viverra ac ipsum in fermentum. Proin at
+        lectus in massa aliquam convallis. Ut mollis nisi vitae est tempus
+        rutrum. Nulla accumsan dui sed enim suscipit dignissim. Aenean sit amet
+        ornare mauris, nec feugiat tortor. Morbi maximus bibendum tempus. Nulla
+        facilisi. Ut tincidunt auctor enim.
+      </p>
+    </div>
+  )
+}
+
+export default TeamPage
